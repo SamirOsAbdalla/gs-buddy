5c2cb10c7037fefc383fb5f1db701af6eae2a740
samir01 <samir.os.abdalla@gmail.com>
2024-01-12 14:04:49 -0800

    refactor: fixed youtube videos issue

--- scripts/content.js
+++ scripts/content.js
@@ -1,157 +1,7 @@
-function foundHistoryItem(dataNode, visitedLink) {
-
-    return (dataNode.url == visitedLink.href || dataNode.title == visitedLink.text)
-}
-
-
-//JSC = jscontroller
-function findJSCAttribute(childNodes) {
-    for (let childNode of childNodes) {
-        if (childNode.getAttribute && childNode.getAttribute("jscontroller")) {
-            return childNode
-        }
-    }
-    return undefined
-}
-
-
-function findNodeWithJSCAtrr(currentNode) {
-    //Three Scenarios
-    //1. One of the child nodes has jscontroller attribute
-    //2. One of the child nodes of the first child node has jscontroller attribute
-    //3. The child node is a youtube video and now we find the jscontroller node with the data-surl attribute
-    if (!currentNode || !currentNode.childNodes) {
-        return undefined
-    }
-
-    //First scenario
-    let returnNode = findJSCAttribute(currentNode.childNodes)
-    if (returnNode) {
-        return returnNode
-    }
-
-    //Second scenario
-    let firstChild = currentNode.firstChild
-    if (!firstChild || !firstChild.childNodes) {
-        return undefined
-    }
-
-    returnNode = findJSCAttribute(firstChild.childNodes)
-    if (returnNode) {
-        return returnNode
-    }
-
-    //Third Scenario
-    while (firstChild && firstChild.getAttribute) {
-        if (firstChild.getAttribute("data-surl")?.startsWith("https://www.youtube.com")) {
-            return firstChild
-        }
-        firstChild = firstChild.firstChild
-    }
-
-    return undefined
-
-}
-
-//BIG LINK STRUCTURE
-//BIG LINK
-// sublink1
-// sublink2
-// .....
-// more results
-
-function getSublinkContainer(currentNode) {
-    let iteratedNode = currentNode.firstChild
-
-    if (!iteratedNode) {
-        return undefined
-    }
-
-    let childNodes = iteratedNode.childNodes
-
-    for (let childNode of childNodes) {
-        let firstChild = childNode.firstChild
-        if (firstChild && firstChild.getAttribute && firstChild.getAttribute("data-ved")) {
-            if (firstChild.getAttribute("role") == "button") {
-                continue
-            }
-            return firstChild.firstChild
-        }
-    }
-    return undefined
-}
-
-function processSublinkContainer({
-    sublinkContainer,
-    jscNode,
-    historyItemsArray,
-    type,
-    parentNode,
-    childNode
-}) {
-    let bigLinkContainer = jscNode.firstChild.firstChild
-
-    let { visitedSublinks, visitedAllSublinks } = processSublinks(sublinkContainer, historyItemsArray, type)
-    let bigLink = processBigLinkContainer(bigLinkContainer, historyItemsArray)
-
-    if (type != "CLEARCLICK") {
-        if (bigLink) {
-            visitedSublinks.push(bigLink)
-        }
-        return visitedSublinks
-    }
-
-    if (!visitedAllSublinks || !bigLink) {
-        return visitedSublinks
-    }
-
-    if (bigLink) {
-        return [childNode]
-    }
-}
-
-function processBigLinkContainer(bigLinkContainer, historyItemsArray) {
-
-    let bigLink = bigLinkContainer
-    while (bigLink && bigLink.tagName != "A") {
-        bigLink = bigLink.firstChild
-    }
-
-    if (!bigLink) {
-        return undefined
-    }
-    if (historyItemsArray.find((dataNode) => foundHistoryItem(dataNode, bigLink))) {
-        return bigLink
-    }
-
-    return undefined
-}
-
-function processSublinks(sublinkContainer, historyItemsArray, type) {
-
-    let sublinkNodes = sublinkContainer.childNodes
-    let pushCount = 0
-    let visitedSublinks = []
-    for (let childNode of sublinkNodes) {
-        let link = childNode.firstChild.firstChild.firstChild
-        if (!historyItemsArray.find((dataNode) => foundHistoryItem(dataNode, link))) {
-            continue
-        }
-
-        if (type == "CLEARCLICK") {
-            pushCount += 1
-            visitedSublinks.push(childNode)
-        } else {
-            visitedSublinks.push(link)
-        }
-    }
-
-    return ({
-        visitedSublinks,
-        visitedAllSublinks: pushCount == sublinkNodes.length || pushCount == sublinkNodes.length - 1
-    })
-}
-
+// need this variable so mutation observer has access to history
+let previousHistoryItemsArray = []
+let autoClearStatus = false
+initializeObserver()
 
 function processNormalNode({
     jscNode,
@@ -232,55 +82,83 @@ function processAskNode({
     return visitedLinks
 }
 
-
-//This is the div with a bunch of images in it
-function processImagesWrapper(imagesWrapper, historyItemsArray) {
-
+function processTable(historyItemsArray) {
+    //handle possible table
+    let table = document.getElementsByTagName("table")
     let visitedLinks = []
+    let allChildrenVisited = false
     let visitedCount = 0
-    imagesWrapper.childNodes.forEach(container => {
+    if (table) {
+        let rows = table[0].rows
+        for (let i = 0; i < rows.length; i++) {
+            if (i == 0) {
+                continue
+            }
 
-        let hasJsAttribute = container.getAttribute("jsname")
-        let imageContainer = hasJsAttribute ? container : container.firstChild
-        let link = imageContainer.childNodes[1].firstChild
-        if (link.tagName != "A" || !historyItemsArray.find(dataNode => foundHistoryItem(dataNode, link))) {
-            return
+            let link = rows[i]
+            while (link && link.tagName != "A") {
+                link = link.firstChild
+            }
+            if (!link) {
+                continue
+            }
+
+            if (historyItemsArray.find((dataNode) => foundHistoryItem(dataNode, link))) {
+                visitedCount += 1
+                visitedLinks.push(rows[i])
+            }
         }
 
-        visitedCount += 1
-        if (hasJsAttribute) {
-            visitedLinks.push(container)
-        } else {
-            visitedLinks.push(imageContainer)
+        allChildrenVisited = (visitedCount == rows.length - 1)
+        if (!allChildrenVisited) {
+            return visitedLinks
         }
-
-    })
-
-    return {
-        visitedLinks,
-        allChildrenVisited: visitedCount > 0 && visitedCount == imagesWrapper.childNodes.length
+        //now check for the main link to see if we remove entire block or just the table links
+        let baseLink = table[0].parentNode.firstChild
+        while (baseLink && baseLink.tagName != "A") {
+            baseLink = baseLink.firstChild
+        }
+        if (!baseLink) {
+            return visitedLinks
+        }
+        if (historyItemsArray.find((dataNode) => foundHistoryItem(dataNode, baseLink))) {
+            return [table[0].parentElement.parentElement.parentElement]
+        }
     }
+
+    return visitedLinks
 }
 
+function handleOutliers(parentNode, childNodes, historyItemsArray) {
+
+    let returnLinks = []
+    {
+        let allVisitedNode = parentNode.childNodes[0]
+        let visitedLinks = handleImagesNode(historyItemsArray, allVisitedNode)
+        visitedLinks?.forEach(visitedLink => {
+            returnLinks.push(visitedLink)
+        })
+    }
+    {
+        let visitedLinks = processTable(historyItemsArray)
+        visitedLinks.forEach(visitedLink => {
+            returnLinks.push(visitedLink)
+        })
+    }
+    return returnLinks
+}
 
 function processChildNodes(parentNode, childNodes, historyItemsArray, type) {
     let returnedNodes = []
 
-    let dataNrNodes = document.querySelectorAll("[data-nr]")
-    if (dataNrNodes.length > 0 && dataNrNodes[0].classList.length == 0) {
-        let { visitedLinks, allChildrenVisited } = processImagesWrapper(dataNrNodes[0], historyItemsArray)
-        if (allChildrenVisited) {
-            returnedNodes.push(parentNode.childNodes[0])
-        } else {
-            visitedLinks?.forEach(visitedLink => {
-                returnedNodes.push(visitedLink)
-            })
-        }
-    }
+    let outlierVisitedLinks = handleOutliers(parentNode, childNodes, historyItemsArray)
+    outlierVisitedLinks.forEach(link => {
+        returnedNodes.push(link)
+    })
 
 
     childNodes.forEach(childNode => {
-        let jscNode = findNodeWithJSCAtrr(childNode) || document.getElementById("iur")
+        let jscNode = findNodeWithJSCAtrr(childNode)
         if (!jscNode) {
             return
         }
@@ -316,6 +194,7 @@ function processChildNodes(parentNode, childNodes, historyItemsArray, type) {
             return
         }
         else {
+
             let visitedLink = processNormalNode({
                 jscNode,
                 historyItemsArray,
@@ -335,29 +214,21 @@ function processChildNodes(parentNode, childNodes, historyItemsArray, type) {
 }
 
 
-function deleteNodes(returnedNodes) {
-
-    returnedNodes.forEach((visitedNode) => {
-
-        //display of none prevents content shift
-        visitedNode.style.display = "none"
-
-        // parentNode.removeChild(childNode)
-    })
-}
-
-function setNodeColors(returnedNodes, chosenColor) {
-    returnedNodes.forEach(returnedNode => { returnedNode.style.color = chosenColor })
-}
-
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, data, chosenColor } = obj
+    const { type, historyItemsArray, chosenColor } = obj
 
+    previousHistoryItemsArray = historyItemsArray
+    if (type == "AUTOCLEARTRUE" || type == "AUTOCLEARFALSE") {
+        autoClearStatus = (type == "AUTOCLEARTRUE" ? true : false)
+        return
+    }
+
+    previousHistoryItemsArray = historyItemsArray
     if (type == "CLEARCLICK") {
         let searchNodesContainerList = document.querySelectorAll("[data-async-context]")
         searchNodesContainerList.forEach(node => {
             if (node.getAttribute("data-async-context").includes("query:")) {
-                let returnedNodes = processChildNodes(node, node.childNodes, data, type)
+                let returnedNodes = processChildNodes(node, node.childNodes, historyItemsArray, type)
                 deleteNodes(returnedNodes)
                 return
             }
@@ -365,16 +236,14 @@ chrome.runtime.onMessage.addListener((obj, sender, response) => {
     } else {
         let allLinks = document.getElementsByTagName("a")
         let returnedNodes = []
-
         for (let i = 0; i < allLinks.length; i++) {
             let link = allLinks[i]
-            if (data.find(historyItem => foundHistoryItem(historyItem, link))) {
+            if (historyItemsArray.find(historyItem => foundHistoryItem(historyItem, link))) {
                 returnedNodes.push(link)
             }
         }
         setNodeColors(returnedNodes, chosenColor)
     }
-
 
     if (type == "COLORCHANGE") {
         chrome.storage.local.set({ "chosenColor": chosenColor })
