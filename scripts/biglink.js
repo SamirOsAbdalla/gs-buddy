da4f386ed99d80fe6dc0e234045faeef5df29698
samir01 <samir.os.abdalla@gmail.com>
2024-01-14 17:30:56 -0800

    refactor: fixed incorrect variable name being supplied to function

--- scripts/biglink.js
+++ scripts/biglink.js
@@ -64,7 +64,7 @@ function processBigLinkContainer(bigLinkContainer, historyItems) {
     if (!bigLink) {
         return undefined
     }
-    if (foundHistoryItem(historyItems, link)) {
+    if (foundHistoryItem(historyItems, bigLink)) {
         return bigLink
     }
 
