4deb291d657b3a154dcff31f7cf220e4209c660c
samir01 <samir.os.abdalla@gmail.com>
2024-01-16 10:36:43 -0800

    refactor: change in history items now issues a clear event when auto clear is checked

--- background.js
+++ background.js
@@ -1,20 +1,51 @@
 const extensionURL = 'https://www.google.com/search';
+async function getActiveTabURL() {
+    const tabs = await chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+
+    return tabs[0];
+}
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-
     if (!tab.url.startsWith(extensionURL)) {
         return
     }
 
-    chrome.storage.local.get("chosenColor", ({ chosenColor }) => {
+    chrome.storage.local.get("autoClear", ({ autoClear }) => {
         chrome.history.search({ text: '', 'maxResults': 1000000000, startTime: 0 }, function (data) {
-            chrome.tabs.sendMessage(tabId, {
-                type: "SETCOLOR",
-                historyItems: data,
-                chosenColor,
-            })
+            if (autoClear) {
+                chrome.tabs.sendMessage(tabId, {
+                    type: "AUTOCLEARTRUE",
+                    historyItems: data,
+                    undefined
+                })
+            } else {
+                chrome.storage.local.get("chosenColor", ({ chosenColor }) => {
+                    chrome.tabs.sendMessage(tabId, {
+                        type: "SETCOLOR",
+                        historyItems: data,
+                        chosenColor,
+                    })
+                })
+            }
         });
-
     })
 })
 
+chrome.history.onVisited.addListener(function (historyItem) {
+    chrome.storage.local.get("autoClear", async ({ autoClear }) => {
+        let activeTab = await getActiveTabURL()
+        let tabId = activeTab.id
+        if (autoClear) {
+            chrome.history.search({ text: '', 'maxResults': 1000000000, startTime: 0 }, function (data) {
+                chrome.tabs.sendMessage(tabId, {
+                    type: "AUTOCLEARTRUE",
+                    historyItems: data,
+                    undefined
+                })
+            })
+        }
+    })
+});
