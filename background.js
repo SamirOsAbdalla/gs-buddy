4f20df3a2136aa533b51140b9aa3193c0e85687d
samir01 <samir.os.abdalla@gmail.com>
2024-01-16 10:45:15 -0800

    refactor: a change in history items now causes a change in chosen link color

--- background.js
+++ background.js
@@ -38,14 +38,22 @@ chrome.history.onVisited.addListener(function (historyItem) {
     chrome.storage.local.get("autoClear", async ({ autoClear }) => {
         let activeTab = await getActiveTabURL()
         let tabId = activeTab.id
-        if (autoClear) {
-            chrome.history.search({ text: '', 'maxResults': 1000000000, startTime: 0 }, function (data) {
+        chrome.history.search({ text: '', 'maxResults': 1000000000, startTime: 0 }, function (data) {
+            if (autoClear) {
                 chrome.tabs.sendMessage(tabId, {
                     type: "AUTOCLEARTRUE",
                     historyItems: data,
                     undefined
                 })
-            })
-        }
+            } else {
+                chrome.storage.local.get("chosenColor", ({ chosenColor }) => {
+                    chrome.tabs.sendMessage(tabId, {
+                        type: "SETCOLOR",
+                        historyItems: data,
+                        chosenColor,
+                    })
+                })
+            }
+        })
     })
 });
